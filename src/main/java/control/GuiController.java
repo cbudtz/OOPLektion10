6c8c4d01506ecd842e6349c7d7785bb1a325f01c
Christian <chrbudtz@gmail.com>
2022-04-03 15:55:44 +0200

    initial

--- src/main/java/control/GuiController.java
+++ src/main/java/control/GuiController.java
@@ -1,19 +1,30 @@
 package control;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.shape.Polyline;
 
 public class GuiController {
+    @FXML
     public Polyline polyLine;
+    @FXML
+    public Label label;
 
     public void buttonClicked(ActionEvent actionEvent) {
-        for (int i = 0; i < 40; i++) {
-            polyLine.getPoints().addAll(i*5.0 , Math.random() * 480);
-            try {
-                Thread.sleep(250);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+
+        new Thread(() -> {
+            for (int i = 0; i < 40; i++) {
+                polyLine.getPoints().addAll(i*5.0 , Math.random() * 480);
+                try {
+                    Thread.sleep(250);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-        }
+            Platform.runLater(()->label.setText("Done Drawing"));
+        }).start();
+
     }
 }
