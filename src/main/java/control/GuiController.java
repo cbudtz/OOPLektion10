a5dfeff00d1ab389f4714bf5659622c0546f6618
Christian <chrbudtz@gmail.com>
2022-04-03 15:32:32 +0200

    initial

--- src/main/java/control/GuiController.java
+++ src/main/java/control/GuiController.java
@@ -7,6 +7,13 @@ public class GuiController {
     public Polyline polyLine;
 
     public void buttonClicked(ActionEvent actionEvent) {
-
+        for (int i = 0; i < 40; i++) {
+            polyLine.getPoints().addAll(i*5.0 , Math.random() * 480);
+            try {
+                Thread.sleep(250);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
